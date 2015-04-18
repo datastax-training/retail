338e4658fa213b31327e7a217ac0d51e701b8125
Ryan Knight <ryan@grandcloud.com>
2015-04-18 17:23:20 -0600

    started working on metagener

--- generate_data/3.scan_data/3.start-metagener.sh
+++ generate_data/3.scan_data/3.start-metagener.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -x # echo on
 
-/cornerstone/tools/datastax/metagener/run --yaml /cornerstone/tools/datastax/metagener/datastax/black-friday/black-friday.yaml
+./run-metagener --yaml black-friday.yaml
