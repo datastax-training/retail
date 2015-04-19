27b6ff242a164d079b93700275628a54547e4a6d
stevelowenthal <steve@datastax.com>
2015-04-19 12:21:15 -0700

    commit before merge

--- generate_data/3.scan_data/4.metagener-to-cassandra-stores-employees.py
+++ generate_data/3.scan_data/4.metagener-to-cassandra-stores-employees.py
@@ -31,7 +31,7 @@ def init_cassandra():
 
     # grab ip address information from application.cfg
     app = Flask(__name__)
-    app.config.from_pyfile('/cornerstone/web/datastax/cornerstone-python/Cornerstone/application.cfg')
+    app.config.from_pyfile('../../web/application.cfg')
     ip_addresses = app.config['DSE_CLUSTER'].split(',')
 
     # connect to Cassandra
