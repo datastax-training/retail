1be6560d77e053f25556c5cb3356c1a397bfcb6a
stevelowenthal <steve@datastax.com>
2015-04-19 19:03:25 -0700

    metagener works!!!

--- generate_data/3.scan_data/3.start-metagener.sh
+++ generate_data/3.scan_data/3.start-metagener.sh
@@ -2,8 +2,11 @@
 set -x # echo on
 
 YAML_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CACHE_DIR=../../cache
 
-sed "s#\\\$YAML_DIR#$YAML_DIR#" $YAML_DIR/black-friday.yaml > ../../cache/black-friday.tmp.yaml
 
-../../tools/metagener/run --yaml ../../cache/black-friday.tmp.yaml
+sed "s#\\\$CACHE_DIR#$CACHE_DIR#" $YAML_DIR/black-friday.yaml > ../../cache/black-friday.yaml
+sed "s#\\\$CACHE_DIR#$CACHE_DIR#" $YAML_DIR/black-friday.metagener > ../../cache/black-friday.metagener
+
+../../tools/metagener/run --yaml $CACHE_DIR/black-friday.yaml
 
