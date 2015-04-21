9692d2dfdeabe6965eefc30c61bc131f5d571ba9
stevelowenthal <steve@datastax.com>
2015-04-21 10:23:15 -0700

    add solr sample

---
+++ solr/add-schema.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -v
+HOST=localhost
+
+if [ "$1" == "-r" ] ; then
+    ACTION=RELOAD
+else
+    ACTION=CREATE
+fi
+   
+echo "Posting solrconfig ..."
+curl --data-binary @solrconfig.xml -H 'Content-type:text/xml; charset=utf-8' "http://$HOST:8983/solr/resource/retail.zipcodes/solrconfig.xml"
+
+echo "Posting schema ..."
+curl --data-binary @zipcodes.xml -H 'Content-type:text/xml; charset=utf-8' "http://$HOST:8983/solr/resource/retail.zipcodes/schema.xml" 
+
+echo "Creating index..."
+curl  -X POST "http://$HOST:8983/solr/admin/cores?action=$ACTION&name=retail.zipcodes"
+echo "Created index."
