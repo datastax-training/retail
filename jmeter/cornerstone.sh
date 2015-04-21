7652c6dfc47f12dfd4ac6a91ff705af54bdbf434
Student <student@cascor.(none)>
2015-04-20 17:28:32 -0700

    Add jmeter

--- jmeter/cornerstone.sh
+++ jmeter/cornerstone.sh
@@ -1,1 +1,1 @@
-./apache-jmeter-2.13.1/bin/jmeter -Jhostname=127.0.0.1 -JnumUsers=1 -p ./bestburgers.jmx -l cornerstone.log
+./apache-jmeter-2.13.1/bin/jmeter -Jhostname=127.0.0.1 -JnumUsers=1 -t ./bestburgers.jmx -l cornerstone.log
---
+++ jmeter/headless_cornerstone.sh
@@ -0,0 +1,1 @@
+./apache-jmeter-2.13.1/bin/jmeter -n -Jhostname=127.0.0.1 -JnumUsers=1 -t ./bestburgers.jmx -l cornerstone.log
