907783bb7d2533ffa45e12619b5a28a8dcea0c44
stevelowenthal <steve@datastax.com>
2015-04-24 12:29:13 -0700

    Add more pages and links

--- web/routes/web.py
+++ web/routes/web.py
@@ -5,8 +5,56 @@ import rest
 web_api = Blueprint('receipts_api', __name__)
 
 get_receipt_by_id_stmt = None
+get_product_by_id_stmt = None
+get_product_by_brand_cc = None
+get_product_by_category_cc = None
 get_receipt_by_cc = None
 
+@web_api.route('/brand')
+def find_products_by_brand():
+
+    global get_product_by_brand_cc
+    global get_product_by_category_cc
+    results = None
+
+    brand_id = request.args.get('brand_id')
+    category_name = request.args.get('category_name')
+
+    if brand_id:
+        if get_product_by_brand_cc is None:
+            get_product_by_brand_cc = rest.session.prepare("SELECT * from retail.products_by_supplier WHERE supplier_id = ?")
+        results = rest.session.execute(get_product_by_brand_cc,[long(brand_id)])
+
+    if category_name:
+        if get_product_by_category_cc is None:
+            get_product_by_category_cc = rest.session.prepare("SELECT * from retail.products_by_category_name WHERE category_name = ?")
+        results = rest.session.execute(get_product_by_category_cc,[category_name])
+
+    return render_template('brand_search.jinja2', products = results)
+
+
+@web_api.route('/product')
+def find_product_by_id():
+
+    global get_product_by_id_stmt
+    results = None
+
+    product_id = request.args.get('product_id')
+
+    if product_id is not None:
+        if get_product_by_id_stmt is None:
+           get_product_by_id_stmt = rest.session.prepare("SELECT * from retail.products_by_id WHERE product_id = ?")
+
+        results = rest.session.execute(get_product_by_id_stmt,[product_id])
+
+        if results is not None:
+            product = results[0]
+        else:
+            product = None
+
+        features = product["features"]
+
+    return render_template('product_search.jinja2', product = product, features=features)
 
 @web_api.route('/receipt')
 def find_receipt_by_id():
@@ -36,7 +84,6 @@ def find_receipt_by_credit_card():
         if get_receipt_by_cc is None:
            get_receipt_by_cc = rest.session.prepare("SELECT * from retail.receipts_by_credit_card WHERE credit_card_number = ?")
 
-        cc_no_long = long(cc_no)
         results = rest.session.execute(get_receipt_by_cc,[long(cc_no)])
 
     return render_template('credit_card_search.jinja2', receipts = results)
