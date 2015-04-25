72c7d454d77ca7a02b08c853ed6d385fbd5a7f81
stevelowenthal <steve@datastax.com>
2015-04-25 00:03:01 -0700

    New retail.cql

--- web/routes/web.py
+++ web/routes/web.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, jsonify, request, render_template
+import urllib
+import urllib2
+import json
+
 # from rest import session
 import rest
 
 web_api = Blueprint('web_api', __name__)
-
-
 
 def init():
     global get_receipt_by_id_stmt
@@ -35,11 +37,10 @@ def find_products_by_brand():
 
     if brand_id:
         results = rest.session.execute(get_product_by_brand_cc,[long(brand_id)])
-
-    if category_name:
+    elif category_name:
         results = rest.session.execute(get_product_by_category_cc,[category_name])
 
-    return render_template('brand_search.jinja2', products = results)
+    return render_template('product_list.jinja2', products = results)
 
 
 @web_api.route('/product')
@@ -47,20 +48,19 @@ def find_product_by_id():
 
     global get_product_by_id_stmt
     results = None
+    product = None
+    features = None
 
     product_id = request.args.get('product_id')
 
     if product_id is not None:
         results = rest.session.execute(get_product_by_id_stmt,[product_id])
 
-        if results is not None:
+        if results:
             product = results[0]
-        else:
-            product = None
+            features = product["features"]
 
-        features = product["features"]
-
-    return render_template('product_search.jinja2', product = product, features=features)
+    return render_template('product_detail.jinja2', product = product, features=features)
 
 @web_api.route('/receipt')
 def find_receipt_by_id():
@@ -73,7 +73,7 @@ def find_receipt_by_id():
     if receipt_id is not None:
         results = rest.session.execute(get_receipt_by_id_stmt,[long(receipt_id)])
 
-    return render_template('receipt_search.jinja2', scans = results)
+    return render_template('receipt_detail.jinja2', scans = results)
 
 @web_api.route('/credit_card')
 def find_receipt_by_credit_card():
@@ -87,3 +87,26 @@ def find_receipt_by_credit_card():
         results = rest.session.execute(get_receipt_by_cc,[long(cc_no)])
 
     return render_template('credit_card_search.jinja2', receipts = results)
+
+@web_api.route('/search')
+def search():
+    # this will search the city field in the retail.zipcodes solr core
+    # the import parameter is 's'
+
+    keyword = request.args.get('s')
+    facet = request.args.get('facet')
+
+    # parameters to solr are rows=30  wt (writer type)=json, and q=city:<keyword> sort=zipcode asc
+    parameters = urllib.urlencode({'rows':'300', 'wt': 'json', 'q': "title:" + keyword})
+    url='http://localhost:8983/solr/retail.products_by_id/select?' + parameters
+
+    # get the response
+    response = urllib2.urlopen(url)
+
+    # fish out the docs from the solr response
+    parsed_response = json.loads(response.read())
+    docs = parsed_response['response']['docs']
+
+    return render_template('product_list.jinja2', products = docs)
+
+
