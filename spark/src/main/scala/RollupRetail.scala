ae96d10402cbddd6e12f8713739bcd365bd4e4d3
stevelowenthal <steve@datastax.com>
2015-04-27 13:11:15 -0700

    Big refactor

--- spark/src/main/scala/RollupRetail.scala
+++ spark/src/main/scala/RollupRetail.scala
@@ -27,14 +27,18 @@ case class Store(
                   zip: Long,
                   size_in_sf: Int)
 
-object Example {
+object RollupRetail {
 
   def main(args: Array[String]) {
+
+//    Create Spark Context
 
     val conf = new SparkConf(true)
       .set("spark.cassandra.connection.host", "127.0.0.1")
 
     val sc = new SparkContext("spark://127.0.0.1:7077", "test", conf)
+
+    // Create some general RDDs
 
     val stores = sc.cassandraTable("retail","stores").select("store_id","address",
       "address_2","address_3","city","state","zip","size_in_sf"
@@ -43,12 +47,15 @@ object Example {
     val receipts = sc.cassandraTable("retail","receipts_by_store_date")
 
     val store_state = stores.map(s => (s.store_id, s.state))
-    val total_receipts_by_store = receipts.map(r => (r.getInt("store_id"), r.getDecimal("receipt_total") )  ).reduceByKey(_+_)
 
+    // Compute Sales by State
+
+    val total_receipts_by_store = receipts.map(r => (r.getInt("store_id"), r.getDecimal("receipt_total") )  ).reduceByKey(_+_)
     total_receipts_by_store.join(store_state).map(r => (r._2._2, r._2._1)).reduceByKey(_+_).saveToCassandra("retail","sales_by_state")
 
-    val receipts_by_date = receipts.map(r => (r.getDate("receipt_date"), r.getDecimal("receipt_total")))
+    // Compute Sales by date
 
+    val receipts_by_date = receipts.map(r => (r.getDate("receipt_date"), r.getDecimal("receipt_total")))
     receipts_by_date.reduceByKey(_+_).saveToCassandra("retail","sales_by_date")
 
 
