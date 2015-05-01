f613fc3020af70fc2abda097bb48f3cb9a02b820
stevelowenthal <steve@datastax.com>
2015-05-01 09:00:35 -0700

    Cleanup

--- sparkstreaming/src/main/scala/JMSReceiver.scala
+++ sparkstreaming/src/main/scala/JMSReceiver.scala
@@ -20,7 +20,7 @@ import com.datastax.spark.connector.streaming._
 import com.datastax.spark.connector.cql.CassandraConnector
 
 
-class JMSReceiver(topic_name: String, connection_url: String) extends Receiver[String](StorageLevel.MEMORY_AND_DISK_2) /* with Logging  */
+class JMSReceiver(topic_name: String, connection_url: String) extends Receiver[String](StorageLevel.MEMORY_AND_DISK_2) with Logging
 {
 
 //  var connection: Connection
@@ -58,68 +58,18 @@ class JMSReceiver(topic_name: String, connection_url: String) extends Receiver[S
 
       }
     })
-
-    while (true) {
-      Thread.sleep(1000)
-    }
-
-    consumer.close()
-    session.close()
-    connection.close()
   }
 
 
   override def onStop(): Unit = {
 
-
+//    consumer.close()
+//    session.close()
+//    connection.close()
   }
 
 }
 
-//object JMSReceiverTester {
-//
-//  def main(args: Array[String]) {
-//
-//    // Create a ConnectionFactory
-//    val connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616")
-//   // val connectionFactory = new ActiveMQConnectionFactory("vm://localhost")
-//
-//    // Create a Connection
-//    val connection = connectionFactory.createConnection()
-//    connection.start()
-//
-////    connection.setExceptionListener(new ExceptionPrinter)
-//
-//    // Create a Session
-//    val session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
-//
-//    // Create the destination (Topic or Queue)
-//    val topic = session.createTopic("GiftCards")
-//
-//    // Create a MessageConsumer from the Session to the Topic or Queue
-//    val consumer = session.createConsumer(topic)
-//
-//    consumer.setMessageListener(new MessageListener {
-//      override def onMessage(message: Message): Unit = {
-//        val str = message match {
-//          case textmsg : TextMessage => textmsg.getText
-//          case _ => "Invalid Message"
-//        }
-//        println(str)
-//      }
-//    })
-//
-//    while (true) {
-//      Thread.sleep(1000)
-//    }
-//
-//    consumer.close()
-//    session.close()
-//    connection.close()
-//  }
-//
-//}
-//
 class ExceptionPrinter extends ExceptionListener {
  def onException (ex: JMSException) {
     System.out.println("JMS Exception occured.  Shutting down client." + ex);
