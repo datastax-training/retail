a3466f0d2dc539c8a19261db5e7020a9a7e777d6
stevelowenthal <steve@datastax.com>
2015-05-06 10:51:30 -0400

    New URL utility

--- web-java/src/main/java/playlist/controller/Utilities.java
+++ web-java/src/main/java/playlist/controller/Utilities.java
@@ -1,10 +1,27 @@
 package playlist.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /**
  * DataStax Academy Sample Application
  * <p/>
  * Copyright 2015 DataStax
  */
 public class Utilities {
+    // The elements are in pairs ("q","\"title:usb\"","fq","somepredicate")
+    public static String makeURL(String url, String ... elements) throws UnsupportedEncodingException {
 
+        StringBuilder s = new StringBuilder(url);
+
+        char separator = '?';
+        for (int i = 0; i < elements.length; i += 2) {
+            s.append(separator)
+                    .append(elements[i])
+                    .append('=')
+                    .append(URLEncoder.encode(elements[i+1],"UTF-8"));
+           separator = '&';
+        }
+        return s.toString();
+    }
 }
